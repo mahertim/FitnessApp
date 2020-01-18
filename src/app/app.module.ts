ccad595e531595a5bc705a94abf6d9cc62234734
Tim Maher <tim@timmaher.dev>
2020-01-18 02:35:08 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './containers/app/app.component';
+import { Store } from 'store';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [Store],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
